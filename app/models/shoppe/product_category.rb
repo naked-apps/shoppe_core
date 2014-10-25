ca7fb5d9d032ae9c9d3ceb617d1595b05f1a0d1e
Brendan Kilfoil <brendan@nakedapps.co.nz>
2014-10-25 19:00:18 +1300

    Add combined_permalink to Shoppe::ProductCategory – allowing a category to return a permalink that is prefixed with each ancestor category's permalink as well.

--- app/models/shoppe/product_category.rb
+++ app/models/shoppe/product_category.rb
@@ -6,10 +6,11 @@ module Shoppe
     # :restrict_with_exception relies on a fix to the awesome_nested_set gem
     # which has been referenced in the Gemfile as we can't add a dependency
     # to a branch in the .gemspec
-    acts_as_nested_set dependent: :restrict_with_exception
+    acts_as_nested_set  dependent: :restrict_with_exception,
+                        after_move: :set_ancestral_permalink
   
     self.table_name = 'shoppe_product_categories'
-  
+    
     # Categories have an image attachment
     attachment :image
   
@@ -31,7 +32,36 @@ module Shoppe
     scope :except_descendants, ->(record) { where.not(id: (Array.new(record.descendants) << record).flatten) }
     
     # Set the permalink on callback
-    before_validation { self.permalink = self.name.parameterize if self.permalink.blank? && self.name.is_a?(String) }
+    before_validation :set_permalink, :set_ancestral_permalink
+    after_save :set_child_permalinks
+
+    def combined_permalink
+      if self.permalink_includes_ancestors && self.ancestral_permalink.present?
+        "#{self.ancestral_permalink}/#{self.permalink}"
+      else
+        self.permalink
+      end
+    end
+
+    private
+
+    def set_permalink
+      self.permalink = self.name.parameterize if self.permalink.blank? && self.name.is_a?(String)
+    end
+
+    def set_ancestral_permalink
+      permalinks = Array.new
+      self.ancestors.each do |category|
+        permalinks << category.permalink
+      end
+      self.ancestral_permalink = permalinks.join '/'
+    end
+
+    def set_child_permalinks
+      self.children.each do |category|
+        category.save!  # save forces children to update their ancestral_permalink
+      end
+    end
   
   end
 end
--- db/migrate/20141023095703_add_nested_to_product_categories.rb
+++ db/migrate/20141023095703_add_nested_to_product_categories.rb
@@ -1,4 +1,4 @@
-class AddNestedToCategories < ActiveRecord::Migration
+class AddNestedToProductCategories < ActiveRecord::Migration
   def up
     add_column :shoppe_product_categories, :parent_id, :integer
     add_column :shoppe_product_categories, :lft, :integer
@@ -8,11 +8,20 @@ class AddNestedToCategories < ActiveRecord::Migration
     add_index :shoppe_product_categories, :lft
     add_index :shoppe_product_categories, :rgt
     
+    add_column :shoppe_product_categories, :ancestral_permalink, :string
+    add_column :shoppe_product_categories, :permalink_includes_ancestors, :boolean, default: false
+
     Shoppe::ProductCategory.reset_column_information
     Shoppe::ProductCategory.rebuild!
   end
 
   def down
+    remove_column :shoppe_product_categories, :ancestral_permalink
+    remove_column :shoppe_product_categories, :permalink_includes_ancestors
+
+    remove_index :shoppe_product_categories, :lft
+    remove_index :shoppe_product_categories, :rgt
+
     remove_column :shoppe_product_categories, :parent_id
     remove_column :shoppe_product_categories, :lft
     remove_column :shoppe_product_categories, :rgt
